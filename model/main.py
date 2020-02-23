e67907998c6810110b65fec92d88964d30fa3187
rizzen <onemangd@gmail.com>
2020-02-24 01:37:59 +0300

    Data exploration

--- model/main.py
+++ model/main.py
@@ -5,7 +5,23 @@ from preprocessing.preprocessor import Preprocessor
 from cnn.cnn_model import CNNModel
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import seaborn as sns
+import numpy as np  # linear algebra
+
+import pandas as pd
 from tensorflow import keras
+
+def explore_data(df_train, df_test, abnormal, normal):
+    fullmitbih = pd.DataFrame(
+        {'Category':pd.concat([df_test, df_train]) [187]})
+    fullptbdb = pd.DataFrame({'Category':pd.concat([abnormal, normal]) [187]})
+
+    sns.countplot(x = "Category", label = "Count", data = fullmitbih)
+    plt.show()
+
+    sns.countplot(x = "Category", label = "Count", data = fullptbdb)
+    plt.show()
+
 
 def vizualize_history(history):
     history_dict = history.history
@@ -28,7 +44,7 @@ def vizualize_history(history):
     plt.clf()
     acc_values = history_dict ['accuracy']
     val_acc = history_dict ['val_accuracy']
-    plt.plot(epochs, acc, 'bo', label = 'Training acc')
+    plt.plot(epochs, acc_values, 'bo', label = 'Training acc')
     plt.plot(epochs, val_acc, 'b', label = 'Validation acc')
     plt.title('Training and validation accuracy')
     plt.xlabel('Epochs')
@@ -54,8 +70,10 @@ def save_model_for_serving(model):
         outputs = {t.name:t for t in model.outputs})
 
 
-# # working with data
+# working with data
 df_train, df_test = Preprocessor.load_mitbih("")
+abnormal, normal = Preprocessor.load_ptbdb("")
+
 X_train, y_train, X_val, y_val, X_test, y_test = Preprocessor.CNN_preprocessor(df_train, df_test, 800, 2000)
 
 # define model
@@ -66,7 +84,7 @@ h_params = CNNModel.CNN_hyperparameters(n_obs)
 model.compile(loss = h_params ['loss'], optimizer = h_params ['optimizer'], metrics = h_params ['metrics'])
 
 history = model.fit(X_train, y_train,
-                    epochs = 1,
+                    epochs = 10,
                     batch_size = h_params ['batch_size'],
                     verbose = 1,
                     validation_data = (X_val, y_val),
@@ -74,7 +92,7 @@ history = model.fit(X_train, y_train,
 
 vizualize_history(history)
 
-save_model_for_serving(model)
+#save_model_for_serving(model)
 #y_pred = model.predict(X_test, batch_size=1000)
 
 print()
