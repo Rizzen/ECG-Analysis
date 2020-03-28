da549cf313b54c08be8dfccf0e47b4fc32f1c5f2
mark.tkachenko <mark.tkachenko@jetbrains.com>
2020-03-28 21:16:59 +0300

    rediscretization

--- ecg-processing/main.py
+++ ecg-processing/main.py
@@ -1,6 +1,7 @@
 import csv
-from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
+from scipy import interpolate
+
 
 def read_floats(array):
     res = []
@@ -8,34 +9,59 @@ def read_floats(array):
         res.append(float(a))
     return res
 
+
 def interpolate_vals(val_left, val_right):
     return (val_left + val_right) / 2
 
-def show_seq(seq):
-    r = range(0, len(seq))
-    plt.plot(r, seq, '.')
 
+def show_seq(x, y):
+    plt.plot(x, y)
     plt.show()
 
-with open('data.csv', newline='') as csvfile:
-    data = list(csv.reader(csvfile))
 
-    nums = read_floats(data[0])
-    print(len(nums))
+def show_seq_dots(x, y):
+    plt.plot(x, y, '.')
+    plt.show()
 
-    res = []
-    i = 1
-    for a in nums:
-        if i >= len(nums):
-            break
-        inter = interpolate_vals(a, nums[i])
-        res.append(a)
-        res.append(inter)
-        i = i + 1
-    res.append(nums[i - 1])
 
-    show_seq(nums)
-    show_seq(res)
+def read_csv_data(filepath: str) -> ([], []):
+    x = []
+    y = []
+    with open(filepath, newline='') as csv_file:
+        data = list(csv.reader(csv_file))
+        for d in data:
+            xd = float(d[0])
+            yd = float(d[1])
+            x.append(xd)
+            y.append(yd)
+    return x, y
 
-print(nums)
-print(res)
+
+def discretize_x(x_arr: [], y_arr: [], frequency: int, t_max: float) -> ([], []):
+    x_max = max(x_arr)
+    modifier = x_max / t_max
+
+    curr = min(y)
+    delta = 1 / frequency
+    iter_count = int(t_max / delta)
+
+    x_new = []
+
+    for i in range(0, iter_count):
+        curr = curr + delta
+        scaled = curr * modifier
+        x_new.append(scaled)
+
+    f = interpolate.interp1d(y, x, bounds_error=False, fill_value='extrapolate')
+    y_new = f(x_new)
+
+    return x_new, y_new
+
+
+x, y = read_csv_data("data2.csv")
+show_seq_dots(y, x)
+
+x_new, y_new = discretize_x(y, x, 125, 9.2)
+
+show_seq_dots(x_new, y_new)
+show_seq(x_new, y_new)
