61dc5381de642758e87780452b210cba873abea3
Mark Tkachenko <mark.tkachenko@jetbrains.com>
2020-04-18 18:58:52 +0300

    files discretizing and shift-slicing

--- ecg-processing/main.py
+++ ecg-processing/main.py
@@ -1,4 +1,6 @@
 import csv
+import glob
+import os
 import matplotlib.pyplot as plt
 from scipy import interpolate
 
@@ -19,8 +21,9 @@ def show_seq(x, y):
     plt.show()
 
 
-def show_seq_dots(x, y):
+def show_seq_dots(x, y, title=""):
     plt.plot(x, y, '.')
+    plt.title(title)
     plt.show()
 
 
@@ -35,6 +38,12 @@ def read_csv_data(filepath: str) -> ([], []):
             x.append(xd)
             y.append(yd)
     return x, y
+
+
+def write_result_to_csv_file(filepath: str,  result:[[]]):
+    with open(filepath, 'w') as result_file:
+        wr = csv.writer(result_file)
+        wr.writerows(result)
 
 
 def discretize_x(x_arr: [], y_arr: [], frequency: int, t_max: float) -> ([], []):
@@ -52,16 +61,64 @@ def discretize_x(x_arr: [], y_arr: [], frequency: int, t_max: float) -> ([], [])
         scaled = curr * modifier
         x_new.append(scaled)
 
-    f = interpolate.interp1d(x_arr, y_arr, bounds_error=False, fill_value='extrapolate')
+    f = interpolate.interp1d(x_arr, y_arr, bounds_error=False, fill_value='extrapolate', kind='slinear')
     y_new = f(x_new)
 
     return x_new, y_new
 
 
-x_data, y_data = read_csv_data("data2.csv")
-show_seq_dots(y_data, x_data)
+def split_to_full_arrays(array: [], array_size: int) -> [[float]]:
+    res = []
+    chunks_num = int(len(array) / array_size)
 
-x_new, y_new = discretize_x(y_data, x_data, 125, 9.2)
+    for i in range(0, chunks_num):
+        arr = list(array[(i * array_size):(i * array_size + array_size)])
+        res.append(arr)
+    return res
 
-show_seq_dots(x_new, y_new)
-show_seq(x_new, y_new)
+
+def slice_and_categorize(array: [], array_size: int, category: int) -> [[]]:
+    split = split_to_full_arrays(array, array_size)
+    for s in split:
+        s.append(category)
+    return split
+
+
+def shift_slice(array: [], array_size: int, shift: int) -> [[float]]:
+    res = []
+    chunks_num = int ((len(array) - array_size) / shift)
+    for i in range(0, chunks_num):
+        arr = list(array[(i * shift):(i * shift + array_size)])
+        res.append(arr)
+    return res
+
+
+def shift_slice_and_categorize(array: [], array_size: int, shift: int, category: int) -> [[]]:
+    split = shift_slice(array, array_size, shift)
+    for s in split:
+        s.append(category)
+    return split
+
+
+#  ['N': 0, 'S': 1, 'V': 2, 'F': 3, 'Q': 4]
+# наджелудочковая - S = 1
+# желудочковая - V = 2
+in_directory = "/Users/mark.tkachenko/Projects/OSS/ECG-Data/raw/наджелудочковая"
+out_file = "/Users/mark.tkachenko/Projects/OSS/ECG-Data/mit-bih-format/ecg-data.csv"
+frequency = 125
+sample_length = 9.2  # seconds
+target_sample_size = 187  # items
+category = 1
+
+os.chdir(in_directory)
+files = glob.glob("*.csv")
+
+res = []
+for file in files:
+    x_data, y_data = read_csv_data(file)
+    x_new, y_new = discretize_x(x_data, y_data, frequency, sample_length)
+    v = shift_slice_and_categorize(y_new, target_sample_size, int(frequency / 2), 1)
+    for arr in v:
+        res.append(arr)
+
+write_result_to_csv_file(out_file, res)
